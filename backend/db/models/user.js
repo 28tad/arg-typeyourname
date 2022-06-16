0c234d3e0f8d5e4d84666559c4061fa1fb5d08b3
Vasya Boeva <vs99516@gmail.com>
2022-06-16 21:47:10 +0300

    backend start

---
+++ backend/db/models/user.js
@@ -0,0 +1,51 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      unique: true,
+
+    },
+    password: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    isActive: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+  });
+  return User;
+};
