d24949b01632581b5c2053f8734b31963a9bf42f
28tad <alimaxcooll@gmail.com>
2022-06-20 20:17:50 +0300

    bug fixes

--- frontend/src/components/intro/Intro.jsx
+++ frontend/src/components/intro/Intro.jsx
@@ -1,16 +1,14 @@
+// REACT
 import React, { useRef, useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom'
 import Draggable  from 'react-draggable'; 
-
-
-import { gsap } from 'gsap';
-// import { CSSPlugin } from 'gsap/CSSPlugin'
-
 // REDUX
 import { useSelector, useDispatch } from 'react-redux';
 import { pullPhrase, countIncrement, fetchPhrases } from '../../storeToolkit/markSlice';
-
+//Animations
+import { gsap } from 'gsap';
 import "./Intro.css"
-
+import "animate.css"
 // MUI 
 import Brightness1Icon from '@mui/icons-material/Brightness1';
 import AlarmIcon from '@mui/icons-material/AccessAlarm';
@@ -37,16 +35,12 @@ function Intro() {
   const sunIcon = useRef(null);
   
   const Mark = useSelector((state) => state.mark);
-  // console.log(Mark);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // dispatch(pullPhrase(['Ты кто такой? ', 'Я тебя не знаю', 'Иди нахуй'])) // FETCHHHH
     dispatch(fetchPhrases(1))
-
-    
-    // dispatch(countIncrement())
   }, [])
-
 
   function expand(e) {
     
@@ -56,10 +50,8 @@ function Intro() {
       gsap.fromTo(circleOutIcon1.current, { scale: 5 },{ scale: 1, duration: 1 });
       gsap.fromTo(circleOutIcon2.current, { scale: 10 },{ scale: 1, duration: 1});
       setCount((prev) => prev + 1)
-   
       
       if(count === 2) {
-        // console.log(Mark.phrase[1][0][0]);
         setDay(false);
         gsap.to(moonIcon.current, {rotation: 360, duration: 1} )
         setTimeout(() => {
@@ -85,8 +77,13 @@ function Intro() {
       gsap.fromTo(circle.current, {scale: 2}, {scale: 1, duration: 1})
       setPhraseBuff(Mark.phrase[0][Mark.count])
 
-    } else if (Mark.count > Mark.phrase[0].length) {
-      // REDIRECT + ANIMATION
+    } else if (Mark.count >= Mark.phrase[0].length) {
+        console.log('11111');
+        gsap.to(circle.current, {x: 500, opacity: 0,duration: 3, onComplete:fadeOut})
+
+        function fadeOut() {
+          navigate('/chapterone/levelone')
+        }
     }
   }
 
@@ -206,5 +203,4 @@ function Intro() {
     
   );
 }
-
 export default Intro;
