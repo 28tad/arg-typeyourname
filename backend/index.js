ac9d82fda0db8501cd55dc3fca4b571a0c9be362
Vasya Boeva <vs99516@gmail.com>
2022-06-20 13:46:34 +0300

    lineRoute.get by sublevel id

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const { Line } = require('./db/models');
+
+async function findLines(id) {
+  const lines = await Line.findAll({ where: { sublevel_id: id }, raw: true, order: [['order', 'ASC']] });
+
+  console.log(lines);
+}
+
+findLines(1);
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const homeRoute = require('./routes/homeRoute');
+const lineRoute = require('./routes/lineRoute');
 const mainConfig = require('./config/config');
 
 const app = express();
 mainConfig(app);
 app.use('/', homeRoute);
+
+app.use('/lines', lineRoute);
 app.listen(4000, () => { console.log('server is up'); });
