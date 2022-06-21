010b580f1d3a50b47c8b0f1502a69870e6d35a26
28tad <alimaxcooll@gmail.com>
2022-06-21 15:48:30 +0300

    merge for bux fixes

--- frontend/src/components/App/App.jsx
+++ frontend/src/components/App/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 // import store from '../../store';
 import Home from '../register/Home.jsx';
 import LevelOne from '../ChapterOne/LevelOne/LevelOne.jsx';
-import LevelTwo from '../ChapterOne/LevelTwo/LevelTwo.jsx';
+import BetweenTwo from '../ChapterOne/BetweenTwo/BetweenTwo.jsx';
 import LevelThree from '../ChapterOne/LevelThree/LevelThree.jsx';
 import Intro from '../intro/Intro';
 import store from '../../storeToolkit/index.js';
@@ -19,7 +19,7 @@ function App() {
           <Route path="/intro" element={<Intro/>}/>
           <Route path="/chapterone/levelone" element={<LevelOne/>}/>
           <Route path="/chapterone/betweenone" element={<BetweenOne/>}/>
-          <Route path="/chapterone/leveltwo" element={<LevelTwo/>}/>
+          <Route path="/chapterone/betweentwo" element={<BetweenTwo/>}/>
           <Route path="/chapterone/levelthree" element={<LevelThree/>}/>
         </Routes>
 
