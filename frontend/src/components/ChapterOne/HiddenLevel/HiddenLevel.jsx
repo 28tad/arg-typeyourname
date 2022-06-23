edc0084dae0b5f19a7c02792773373ea89f2f287
28tad <alimaxcooll@gmail.com>
2022-06-23 17:10:09 +0300

    update dialgoue

--- frontend/src/components/ChapterOne/HiddenLevel/HiddenLevel.jsx
+++ frontend/src/components/ChapterOne/HiddenLevel/HiddenLevel.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { countIncrement, fetchPhrases, deletePhrase } from '../../../storeToolkit/markSlice';
+import Brightness1Icon from '@mui/icons-material/Brightness1';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
 
 function HiddenLevel() {
+
+  const circle = useRef(null);
+
+  const Mark = useSelector((state) => state.mark);
+  const dispatch = useDispatch();
+
+  function dialogue(e) {
+    
+    console.log(Mark.phrase);
+  }
+  useEffect(() => {
+    dispatch(fetchPhrases(2))
+  }, [])
+
   return (
-    <div>
-      
+    <div style={{ color: "white"}}>
+      <Brightness1Icon
+          style={{ color: "white"}}
+          ref={circle}
+          sx={{
+            position: 'absolute',
+            left: '49%'
+          }}
+          onClick={dialogue}
+        />
     </div>
   );
 }
