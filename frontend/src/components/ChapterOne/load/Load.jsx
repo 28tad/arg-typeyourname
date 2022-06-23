2ff51cba1859066186aeda7c9b9380a1aa3f7b84
John <nextadm@mail.ru>
2022-06-23 10:10:24 +0300

    New Load.jsx

--- frontend/src/components/ChapterOne/load/Load.jsx
+++ frontend/src/components/ChapterOne/load/Load.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { TailSpin, BallTriangle, Bars, Grid, Puff, Oval, ThreeDots, Circles, Rings} from  'react-loader-spinner'
+import { useNavigate } from 'react-router-dom';
 
 function Load() {
+
+  const navigate = useNavigate()
+  
+    // setTimeout(() => {
+    //   navigate('/')
+    // }, 3000);
+
   return (
-    <div>
-      HUI
+
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: '450px',
+      letterSpacing: '30px',
+      }}>
+
+     <TailSpin color="#d8d0ef" height={80} width={80} />
+     <BallTriangle color="#dbdbdb" height={80} width={80} />
+     <Bars color="#f6f6f6" height={80} width={80} />
+     <Puff color="#dbdbdb" height={80} width={80} /> 
+     <ThreeDots color="#dbdbdb" height={80} width={80} />
+     <Circles color="#dbdbdb" height={80} width={80}/>
+     <Rings color="#dbdbdb" height={80} width={80} />
     </div>
   );
 }
